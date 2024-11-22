30081869647b5389666a64ad72d5f9e907e4736a
juliamenegassi <julia.menegass159@gmail.com>
2024-11-22 13:51:51 -0300

    commit

--- src/Pages/Sono.js
+++ src/Pages/Sono.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react'
 import { View, StyleSheet, Text, StatusBar, TouchableOpacity } from 'react-native'
+import Header from '../Components/Header';
+import SonoHoras from '../Components/SonoHoras';
 
 export default function Sono() {
 
   const [ opcao, setOpcao ] = useState();
+  const [proximo, setProximo] = useState(false);
+
+  if (proximo) {
+    return (<SonoHoras setProximo={setProximo} />)
+}
 
   return (
     <View style={css.cor}>
@@ -13,23 +20,26 @@ export default function Sono() {
       <Text style={css.sono}>Sono</Text>
       </View>
       <View style={css.boxFrase}>
-        <Text style={css.frase}>Como você dormiu na noite do dia 08/11/2024?</Text>
+        <Text style={css.frase}>Como você dormiu na noite do dia 01/01/2024?</Text>
       </View>
       <View style={css.boxBotao}>
-        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Muito bem" ? "red" : "blue"}]} onPress={() => setOpcao( "Muito bem" )}>
-          <Text style={css.botaoTexto}>Muito bem</Text>
+        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Muito bem" ? "#4E778B" : "white"}]} onPress={() => setOpcao( "Muito bem" )}>
+          <Text style={[css.botaoTexto, { color: opcao == "Muito bem" ? "white" : "black"}]} onPress={() => setOpcao( "Muito bem" )}>Muito bem</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={css.botao}>
-          <Text style={css.botaoTexto}>Bem</Text>
+        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Bem" ? "#4E778B" : "white"}]} onPress={() => setOpcao( "Bem" )}>
+          <Text style={[css.botaoTexto, { color: opcao == "Bem" ? "white" : "black"}]} onPress={() => setOpcao( "Bem" )}>Bem</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={css.botao}>
-          <Text style={css.botaoTexto}>Nem bem, nem mal</Text>
+        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Nem bem, nem mal" ? "#4E778B" : "white"}]} onPress={() => setOpcao( "Nem bem, nem mal" )}>
+          <Text style={[css.botaoTexto, { color: opcao == "Nem bem, nem mal" ? "white" : "black"}]} onPress={() => setOpcao( "Nem bem, nem mal" )}>Nem bem, nem mal</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={css.botao}>
-          <Text style={css.botaoTexto}>Mal</Text>
+        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Mal" ? "#4E778B" : "white"}]} onPress={() => setOpcao( "Mal" )}>
+          <Text style={[css.botaoTexto, { color: opcao == "Mal" ? "white" : "black"}]} onPress={() => setOpcao( "Mal" )}>Mal</Text>
         </TouchableOpacity>
-        <TouchableOpacity style={css.botao}>
-          <Text style={css.botaoTexto}>Muito mal</Text>
+        <TouchableOpacity style={[css.botao, { backgroundColor: opcao == "Muito mal" ? "#4E778B" : "white"}]} onPress={() => setOpcao( "Muito mal" )}>
+          <Text style={[css.botaoTexto, { color: opcao == "Muito mal" ? "white" : "black"}]} onPress={() => setOpcao( "Muito mal" )}>Muito mal</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={css.proximo} onPress={() => setProximo(true)}>
+          <Text style={css.seta}>➜</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -54,7 +64,7 @@ const css = StyleSheet.create({
   boxFrase: {
     width: "100%",
     display: "flex",
-    height: 120,
+    height: 100,
     alignItems: "center",
     justifyContent: "center"
   },
@@ -84,6 +94,19 @@ const css = StyleSheet.create({
     height: "100%",
     alignItems: "center"
   }, 
-
+  proximo: {
+    width: "15%",
+    height: 40,
+    borderRadius: 10,
+    marginTop: 10,
+    backgroundColor: "#4E778B",
+    left: 123
+  },
+  seta: {
+    color: "white",
+    textAlign: "center",
+    top: 5,
+    fontSize: 20
+  }
 })
 
